74bbd2adf1d16e19b3e3dba13dcb8d93efad75ac
Darrel Nguyen <Darrelnguyen03@gmail.com>
2023-03-15 12:46:13 -0600

    changes

--- Programming Strategies/part 1.2.py
+++ Programming Strategies/part 1.2.py
@@ -14,28 +14,19 @@ while True:
 
     if category > 5:
         print('Invalid input, please enter a valid number')
-
-    elif category == 1:
-        quantity1 = int(input('Enter quantity sold:\n    '))
-        sum += (models['apple phone']*quantity1)
-
-    elif category == 2:
-        quantity2 = int(input('Enter quantity sold:\n    '))
-        sum += (models['android phone']*quantity2)
-
-    elif category == 3:
-        quantity3 = int(input('Enter quantity sold:\n    '))
-        sum += (models['apple tablet']*quantity3)
-
-    elif category == 4:
-        quantity4 = int(input('Enter quantity sold:\n    '))
-        sum += (models['android tablet']*quantity4)
-
-    elif category == 5:
-        quantity5 = int(input('Enter quantity sold:\n    '))
-        sum += (models['windows tablet']*quantity5)
-
-    if category == 0:
-        sum = sum
-        print('your total profit today is:',sum)
-        break
+    else:
+        quantity = int(input('Enter quantity sold:\n    '))
+        if category == 1:
+            sum += (models['apple phone']*quantity)
+        elif category == 2:
+            sum += (models['android phone']*quantity)
+        elif category == 3:
+            sum += (models['apple tablet']*quantity)
+        elif category == 4:
+            sum += (models['android tablet']*quantity)
+        elif category == 5:
+            sum += (models['windows tablet']*quantity)
+        elif category == 0:
+            sum = sum
+            print('your total profit today is:',sum)
+            break
