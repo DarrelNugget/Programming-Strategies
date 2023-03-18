4c276d19bdafc55db521d9ae92296d176a8bb08a
Gabreal Kalaf <GabrealKalaf22@gmail.com>
2023-03-18 15:58:09 -0600

    fixed a little bit of spacing

--- Programming Strategies/part 2.1.py
+++ Programming Strategies/part 2.1.py
@@ -74,12 +74,12 @@ while True:
              #added message for high sums
                 if category == 0 and sum >= 10000:
                     goodMsg = "You did well this period! Keep up the great work!"
-                    print(f'Your total profit {named_days_value} is: {sum:.2f}' + goodMsg)
+                    print(f'Your total profit {named_days_value} is: {sum:.2f}\n ' + goodMsg)
                     break
                 #added message for low sums
                 if category == 0 and sum < 10000:
                     badMsg = "We didn't reach our goal for this period. More work is needed!"
-                    print(f'Your total profit {named_days_value} is: {sum:.2f}' + badMsg)
+                    print(f'Your total profit {named_days_value} is: {sum:.2f}\n ' + badMsg)
                     break
                     
 
