50af457600a5e1c4cbcc3d1055f021057b06e92c
Gabreal Kalaf <GabrealKalaf22@gmail.com>
2023-03-18 15:43:21 -0600

    made a lot of changes
    looped the welcome statement
    
    looped thw whole program so it wont stop until you tell it to from the day select menu
    
    moved some if statements around
    
    furthered the specififc day option
    
    stopped the for */insert day/* from looping constantly
    
    and the /*for day thin/* is not done yet still figuring that one out

--- Programming Strategies/part 2.1.py
+++ Programming Strategies/part 2.1.py
@@ -6,7 +6,7 @@ models = {
     'windows tablet': 51.49,
 }
 
-spec_days = {
+named_days_value = {
     'Monday':0,
     'Tuesday':0,
     'Wednesday':0,
@@ -16,59 +16,63 @@ spec_days = {
     'Sunday':0,
 }
 
-day_value = 0
-
 print('Welcome to Circle Phones Profit calculator\n')
-print('You can calculate the profit of the company according to a specific day or by a week or divide the week into weekdays and weekend\n')
-
-
-days = int(input('''
-Enter:
-1-For a specific day
-2-For the week
-3-For the week buisness days
-4-For weekend days
-0-Exit
-
-'''))
-
-named_days = (input('''
-Enter a specific day [Monday, Tuesday, 
-Wednesday, Thursday, Friday, Saturday, 
-Sunday]
-
-'''))
-
-
+#looped the welcome
 
 #moved the days_value into while loop below
 
+day_value = 0
+
 while True:
 
+    print('You can calculate the profit of the company according to a specific day or by a week or divide the week into weekdays and weekend')
+    days = int(input('''
+    Enter:
+    1-For a specific day
+    2-For the week
+    3-For the week buisness days
+    4-For weekend days
+    0-Exit
 
+    '''))
 
-    if (days > 4):
+    if(days == 0):#stop the program
+        break
+    elif (days > 4):
         print('invalid input, please enter a valid number')
-    elif(days == 1):#one day
+
+
+    #if (days > 4):
+        #print('invalid input, please enter a valid number')
+        #moved this line up 
+
+
+    if(days == 1):#one day
         day_value+=1
+
+        spec_days = (input('''
+    Enter a specific day [Monday, Tuesday, 
+    Wednesday, Thursday, Friday, Saturday, 
+    Sunday]
+
+    '''))
     elif(days == 2):#whole week
         day_value+=7
     elif(days == 3):#work week
         day_value+=5
     elif(days == 4):#weekends
         day_value+=2
-    elif(days == 0):#stop the program
-        break
 
     sum = 0
-    for i in range(1,day_value):
+    for i in range(0,day_value):
+        print(f'for {named_days_value.keys()}\n')
         while True:
-            print(f'for {named_days}\n')
+            #so the for whatever day doesnt repeat, its been moved out of the while loop
             category = int(input('Enter product number 1-5, or enter 0 to stop:\n'))
 
             if 0 <= category < 6:
                 if category == 0:
-                    print('your total profit today is:',sum)
+                    print('your total profit today is:',sum,'\n')
                     break
                 quantity = int(input('Enter quantity sold:\n'))
 
