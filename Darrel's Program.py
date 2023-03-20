3954689372c1bf242b87a4db1c390613457274c9
Darrel Nguyen <Darrelnguyen03@gmail.com>
2023-03-19 18:17:29 -0600

    Finished Product with commets

--- Darrel's Program.py
+++ Darrel's Program.py
@@ -8,43 +8,81 @@ models = {
 
 days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
+programRunning = True
+
+
+
 print("Welcome to Circle Phones Profit' calculator. You can calculate your profits for a specific day,\nby week or you can divide the week into weekdays and the weekend.")
 print("Welcome to Circle Phones Profit Calculator\n")
-startingDay = 0
-endingDay = 0
 
-while True:
+while programRunning == True:
     sum = 0
-    runs = 0
+    startingDay = 0
+    endingDay = 0
     print("You can calculate the profit of the company according to a specific day or by a week or divide the week into weekdays and weekend")
-    daySelection = int(input("Enter:\n1 - For specific Day\n2 - For the Week\n3 - For Week Business Days\n4 - For Weekend days\n0 - Exit\n"))
-    
-    if daySelection == 0:
-        break
-    elif daySelection == 1: #Single Day
-        specificDay = input("Enter a specific day [Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday]\n")
-        if specificDay == "Monday":
+    #Loop to determine profit of specific days or weeks or weekends..... etc
+    while True:
+        daySelection = int(input("Enter:\n1 - For specific Day\n2 - For the Week\n3 - For Week Business Days\n4 - For Weekend days\n0 - Exit\n"))
+        #Picking one will set the range of the list 
+        if daySelection == 0:
+            programRunning = False
+            break
+        elif daySelection == 1: #Single Day
+            specificDay = input("Enter a specific day [Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday]\n")
+            if specificDay == "Monday":
+                startingDay = 0
+                endingDay = 1
+            elif specificDay == "Tuesday":
+                startingDay = 1
+                endingDay = 2
+            elif specificDay == "Wednesday":
+                startingDay = 2
+                endingDay = 3
+            elif specificDay == "Thursday":
+                startingDay = 3
+                endingDay = 4
+            elif specificDay == "Friday":
+                startingDay = 4
+                endingDay = 5
+            elif specificDay == "Saturday":
+                startingDay = 5
+                endingDay = 6
+            elif specificDay == "Sunday":
+                startingDay = 6
+                endingDay = 7
+            break
+        elif daySelection == 2: #Whole Week
             startingDay = 0
-            endingDay = 1
-    elif daySelection == 2: #Whole Week
-        startingDay = 0
-        endingDay = 7
-    elif daySelection == 3: #Business Days
-        startingDay = 0
-        endingDay = 5
-    elif daySelection == 4: #Weekends
-        startingDay = 5
-        endingDay = 7    
-    else: #this needs to be fix =======================================
-        print("invalid input, please enter a valid number\n")
-    
+            endingDay = 7
+            break
+        elif daySelection == 3: #Business Days
+            startingDay = 0
+            endingDay = 5
+            break
+        elif daySelection == 4: #Weekends
+            startingDay = 5
+            endingDay = 7
+            break
+        else:
+            print("invalid input, please enter a valid number\n")
+
+    #Set the message for profit depending on the daySelection
+    if daySelection == 1:
+        profitMessage = specificDay
+    elif daySelection == 2:
+        profitMessage = "week"
+    elif daySelection == 3:
+        profitMessage = "week (business days)"
+    elif daySelection == 4:
+        profitMessage = "weekend"
+
+    #Loop to calc the profit total
     for i in days[startingDay:endingDay]:
         print(f"For {i}")
-        runs += 1
         while True:
             
             productCategory = int(input('Enter product number 1-5, or enter 0 to stop:\n'))
-
+            #Calc the sum depending on the model picked and amount sold
             if 0 <= productCategory < 6:
                 if productCategory == 0:
                         break
@@ -61,11 +99,13 @@ while True:
                     sum += (models['windows tablet']*quantity)
             else:
                 print('Invalid input, please enter a valid number')
-
-        if runs == endingDay:
+        #Determines if the sum is greater or less than 10000
+        if (days.index(i) + 1) == endingDay:
             if sum >= 10000:
-                goodMsg = "You did well this period! Keep up the great work!\n"
-                print(f"Your total profit for {i} is: {sum:.2f}\n" + goodMsg)
+                goodMsg = (f"You did well this {profitMessage}! Keep up the great work!\n")
+                print(f"Your total profit for the {profitMessage} is: {sum:.2f}\n" + goodMsg)
             elif sum < 10000:
-                badMsg = "We didn't reach our goal for this period. More work is needed!\n"
-                print(f"Your total profit for {i} is: {sum:.2f}\n" + badMsg)
+                badMsg = (f"We didn't reach our goal for this {profitMessage}. More work is needed!\n")
+                print(f"Your total profit for the {profitMessage} is: {sum:.2f}\n" + badMsg)
+
+print("Program End!")
